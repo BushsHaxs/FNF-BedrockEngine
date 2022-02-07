2d6434b182b15be8764e6eb1512ec6db962826d1
Tahir <84905346+TahirArch@users.noreply.github.com>
2022-02-07 10:02:00 +0300

    cringe ass unfunctional json editor (just imported things)
    i wrote this from school lmao

---
+++ source/editors/JsonEditor.hx
@@ -0,0 +1,13 @@
+package editors;
+
+#if desktop
+import Discord.DiscordClient;
+#end
+import flixel.FlxG;
+import flixel.addons.ui.FlxInputText;
+import flixel.text.FlxText;
+
+class JsonEditorAppearance
+{
+      
+}
