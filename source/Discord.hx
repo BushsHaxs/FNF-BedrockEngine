61ae9abcd9abf52f6517be7ff352dbeaf98232c0
Gui iago∞ <45212377+Gui-iago@users.noreply.github.com>
2022-02-08 19:25:53 -0400

    uh

--- source/Discord.hx
+++ source/Discord.hx
@@ -16,7 +16,7 @@ class DiscordClient
 	{
 		trace("Discord Client starting...");
 		DiscordRpc.start({
-			clientID: "client_ID",
+			clientID: client_ID,
 			//clientID: "863222024192262205",
 			onReady: onReady,
 			onError: onError,
