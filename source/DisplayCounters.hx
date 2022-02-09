6ab1dd14e083633e6f80e0659257a571fcb94789
Gui iago∞ <45212377+Gui-iago@users.noreply.github.com>
2022-02-09 15:56:40 -0400

    memory counter is just weird

--- source/DisplayCounters.hx
+++ source/DisplayCounters.hx
@@ -1,35 +1,41 @@
 package;
 
-import openfl.Lib;
-import openfl.display.Bitmap;
-import openfl.display.BitmapData;
-import flixel.FlxG;
-import openfl.events.EventType;
-import openfl.display.DisplayObject;
 import haxe.Timer;
-import openfl.display.FPS;
-import openfl.events.Event;
+import openfl.Lib;
 import openfl.system.System;
 import openfl.text.TextField;
 import openfl.text.TextFormat;
+import openfl.events.Event;
 
 class DisplayCounters extends TextField
 {
+	//standard FPS_Mem stuff
 	private var times:Array<Float>;
 	private var memPeak:Float = 0;
 
-	public function new(inX:Float = 10.0, inY:Float = 10.0, inCol:Int = 0x000000)
+	// display info
+	public static var displayFps = true;
+	public static var displayMemory = true;
+	public static var displayState = true;
+	public static var displayPeak = true;
+
+	private var display:Bool = false;
+
+	public function new(inX:Float = 10.0, inY:Float = 10.0, color:Int = 0x000000, counters:Bool = false)
 	{
 		super();
-		x = inX;
-		y = inY;
+
+		display = counters;
+
+		this.x = x;
+		this.y = y;
+
 		selectable = false;
-		defaultTextFormat = new TextFormat("VCR OSD Mono", 16, inCol);
-		text = "FPS: ";
+		defaultTextFormat = new TextFormat("VCR OSD Mono", 16, color);
 		times = [];
 		addEventListener(Event.ENTER_FRAME, onEnter);
-		width = 1280;
-		height = 720;
+		width = Main.gameWidth;
+		height = Main.gameHeight;
 	}
 
 	private function onEnter(_)
@@ -42,56 +48,29 @@ class DisplayCounters extends TextField
 		if (mem > memPeak)
 			memPeak = mem;
 
-		//will make this crap better in the future, rn it sucks.
 		if (visible)
 		{
-			//1 counter
-			text = "FPS: " + times.length; //FPS Only
+			text = "";
+			
+			if(ClientPrefs.showFPS)
+				text += "FPS: " + times.length + "\n";
 
-			if(ClientPrefs.memCounter && !ClientPrefs.memPeak && !ClientPrefs.showFPS) //Mem Only
-				text = "Memory: " + mem + " mb";
+			if(ClientPrefs.memCounter)
+				text += "Memory: " + mem + " mb" + "\n";
 
-			if(ClientPrefs.memPeak && !ClientPrefs.memCounter && !ClientPrefs.showState && !ClientPrefs.showFPS) //Peak Only
-                text = "Memory Peak: " + memPeak;
+			if(ClientPrefs.memPeak)
+                text += "Memory Peak: " + memPeak + " mb" + "\n";
 
-			if(ClientPrefs.showState && !ClientPrefs.showFPS && !ClientPrefs.memCounter && !ClientPrefs.memPeak) //State Only
-                text = "State: " + Main.curStateS;
-
-
-			//2 counters
-			if(ClientPrefs.showFPS && ClientPrefs.memCounter && !ClientPrefs.memPeak && !ClientPrefs.showState) //FPS and Mem
-				text = "FPS: " + times.length + "\nMemory: " + mem + " mb";
-
-			if(ClientPrefs.showFPS && !ClientPrefs.memCounter && ClientPrefs.memPeak && !ClientPrefs.showState) //FPS and Mem
-				text = "FPS: " + times.length + "\nMemory Peak: " + memPeak + " mb";
-
-			if(ClientPrefs.memCounter && ClientPrefs.memPeak && !ClientPrefs.showFPS) //Mem and Peak
-				text = "Memory: " + mem + " mb\nMemory Peak: " + memPeak + " mb";
-
-            if(ClientPrefs.showState && ClientPrefs.showFPS && !ClientPrefs.memCounter && !ClientPrefs.memPeak) //FPS and State
-                text = "FPS: " + times.length + "\nState: " + Main.curStateS;
-
-			if(ClientPrefs.showState && !ClientPrefs.showFPS && ClientPrefs.memCounter && !ClientPrefs.memPeak) //Mem and State
-                text = "Memory: " + mem + " mb " + "\nState: " + Main.curStateS;
-
-			if(ClientPrefs.showState && !ClientPrefs.showFPS && !ClientPrefs.memCounter && ClientPrefs.memPeak) //Peak and State
-                text = "Memory Peak: " + memPeak + "\nState: " + Main.curStateS;
-
-			//3 counters
-			if(ClientPrefs.showState && ClientPrefs.showFPS && ClientPrefs.memCounter && !ClientPrefs.memPeak) //FPS, Mem, and State
-				text = "FPS: " + times.length + "\nMemory: " + mem + " mb" + "\nState: " + Main.curStateS;
-
-			if(ClientPrefs.showState && ClientPrefs.showFPS && !ClientPrefs.memCounter && ClientPrefs.memPeak) //FPS, Peak, and State
-				text = "FPS: " + times.length + "\nMemory Peak: " + memPeak + " mb" + "\nState: " + Main.curStateS;
-
-			//all
-            if(ClientPrefs.showState && ClientPrefs.showFPS && ClientPrefs.memCounter && ClientPrefs.memPeak) //FPS, Mem, Peak, and State
-            	text = "FPS: " + times.length + "\nMemory: " + mem + " mb\nMemory Peak: " + memPeak + " mb" + "\nState: " + Main.curStateS;
-
-
-			//all disabled
-			if(!ClientPrefs.showFPS && !ClientPrefs.memCounter && !ClientPrefs.memPeak && !ClientPrefs.showState)
-				text = "";
+			if(ClientPrefs.showState)
+                text += "State: " + Main.curStateS;
 		}
 	}
-} 
+
+public static function updateDisplayInfo()
+	{
+		displayFps = ClientPrefs.showFPS;
+		displayState = ClientPrefs.showState;
+		displayMemory = ClientPrefs.memCounter;
+		displayPeak = ClientPrefs.memPeak;
+	}
+}
