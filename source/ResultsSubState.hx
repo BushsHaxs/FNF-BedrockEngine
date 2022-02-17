9c8bb6ed789b77a947fbcfc183d29fe25bf09171
HiroMizuki <72695242+HiroMizuki@users.noreply.github.com>
2022-02-17 20:06:02 -0300

    compile error (W.I.P)

--- source/ResultsSubState.hx
+++ source/ResultsSubState.hx
@@ -34,9 +34,9 @@ class ResultsSubState extends MusicBeatSubstate
 
         var ratings:FlxSpriteGroup = new FlxSpriteGroup;
 
-        accText = new FlxText(-200, 65, 0,
+        /*accText = new FlxText(-200, 65, 0,
             'Accuracy: ${countAcc(PlayState.Highscore.floorDecimal(ratingPercent * 100, 2))}'
-        );
+        );*/
 
         var perfect:FlxSprite = new FlxSprite(-150, 65).loadGraphic(Paths.image('maniamode/resultsscreen/perfect'));
         perfect.antialiasing = true;
